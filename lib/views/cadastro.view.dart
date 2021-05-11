cbca0c6e50060c396e90709de0418c523d44b1a6
{name} <leobuosi09@hotmail.com>
2021-05-11 15:55:56 -0300

    Atualização provider

---
+++ lib/views/cadastro.view.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+
+import 'package:contatos_app/models/contato.model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+
+import '../repositories/contato.repository.dart';
+import '../repositories/contato.repository.dart';
+
+class CadastroView extends StatelessWidget {
+  Contato contato = new Contato();
+  var _formKey = GlobalKey<FormState>();
+
+  Future<void> salvar(
+      BuildContext context, ContatoRepository repository) async {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      if (await repository.create(contato)) {
+        Navigator.of(context).pop();
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Falha no cadastro"),
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var repository = Provider.of<ContatoRepository>(context, listen: true);
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Novo Contato"),
+        ),
+        body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Nome",
+                    ),
+                    validator: (value) =>
+                        value.isEmpty ? "Campo obrigatório" : null,
+                    onSaved: (value) => contato.nome = value,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Telefone",
+                    ),
+                    validator: (value) =>
+                        value.isEmpty ? "Campo obrigatório" : null,
+                    onSaved: (value) => contato.telefone = value,
+                  ),
+                  Container(
+                      margin: const EdgeInsets.only(top: 20),
+                      width: double.infinity,
+                      child: RaisedButton(
+                        child: Text("Salvar"),
+                        onPressed: () => salvar(context, repository),
+                      ))
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/views/lista.view.dart
@@ -0,0 +1,32 @@
+import 'package:contatos_app/models/contato.model.dart';
+import 'package:contatos_app/repositories/contato.repository.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ListaView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+      ),
+      body: Consumer<ContatoRepository>(builder: (context, repository, child) {
+        var contatos = repository.contatos;
+        return ListView.builder(
+          itemCount: contatos.length,
+          itemBuilder: (_, index) {
+            Contato contato = contatos[index];
+            return ListTile(
+              title: Text(contato.nome),
+              subtitle: Text(contato.telefone),
+            );
+          },
+        );
+      }),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => Navigator.of(context).pushNamed('/cadastro'),
+      ),
+    );
+  }
+}
